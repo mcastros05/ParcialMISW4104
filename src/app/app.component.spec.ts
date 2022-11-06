c069a70a58629a960a2500e837155dc5edb7734d
Melissa Castro <melissa.castrosarmiento@teleperformance.com>
2022-11-05 21:43:03 -0500

    tests del componente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlantaListComponent } from './planta/planta-list/planta-list.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        PlantaListComponent
       ],
     }).compileComponents();
   });
@@ -18,18 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'parcial'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('parcial');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('parcial app is running!');
   });
 });
--- src/app/planta/planta-list/planta-list.component.spec.ts
+++ src/app/planta/planta-list/planta-list.component.spec.ts
@@ -2,15 +2,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { PlantaListComponent } from './planta-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Planta } from '../planta'
 
 describe('PlantaListComponent', () => {
   let component: PlantaListComponent;
   let fixture: ComponentFixture<PlantaListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ PlantaListComponent ]
     })
     .compileComponents();
@@ -19,10 +24,33 @@ describe('PlantaListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantaListComponent);
     component = fixture.componentInstance;
+
+    for (let i = 0; i < 3; i++){
+      const planta = new Planta(
+        faker.datatype.number(),
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.lorem.word(),
+        faker.datatype.number(),
+        faker.lorem.word(),
+        faker.lorem.word()
+      );
+      component.plantas.push(planta);
+    }
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have 1 <div.header> elements', () => {
+    expect(debug.queryAll(By.css('div.header'))).toHaveSize(1);
+  });
+
+  it('should have 12 <div.planta> elements', () => {
+    expect(debug.queryAll(By.css('div.planta'))).toHaveSize(12);
+  });
+
 });
